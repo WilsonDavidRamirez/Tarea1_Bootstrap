b8c38a8d7b10a51900df22e756a006ee186e6b10
Wilson R <wdramirez@espe.edu.ec>
2022-05-27 23:45:54 -0500

    Segundo

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__)                       
 
 @app.route('/')
-def principal():
+def principal():                            #Llamamiento al html
     return  render_template('home.html');
 
-if __name__ == '__main__':
+if __name__ == '__main__':                 #Ejecucion del Main
     app.run(debug=True, port=5000)
